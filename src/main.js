242cc69dcc523bd2ae5e398c9b0aa893b2608080
Kevin Grandon <kevingrandon@yahoo.com>
2018-09-20 21:56:10 -0700

    Add meta

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 // @flow
 import App from 'fusion-react';
 import Router from 'fusion-plugin-react-router';
+import HelmetPlugin from 'fusion-plugin-react-helmet-async';
 import Styletron from 'fusion-plugin-styletron-react';
 
 import root from './root.js';
 
 export default () => {
   const app = new App(root);
+  app.register(HelmetPlugin);
   app.register(Styletron);
   app.register(Router);
   return app;
--- src/root.js
+++ src/root.js
@@ -1,15 +1,25 @@
 // @flow
 import React from 'react';
+import {Helmet} from 'fusion-plugin-react-helmet-async';
 import {Route, Switch} from 'fusion-plugin-react-router';
 
 import Home from './pages/home.js';
 import PageNotFound from './pages/pageNotFound.js';
 
 const root = (
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route component={PageNotFound} />
-  </Switch>
+  <div>
+    <Helmet>
+      <title>AlienRides Electric Vehicles &amp; Green Transportation</title>
+      <meta
+        name="description"
+        content="AlienRides is promoting electric vehicle use including electric scooters, electric skateboards, electric unicycles and any kind of electric vehicle."
+      />
+    </Helmet>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route component={PageNotFound} />
+    </Switch>
+  </div>
 );
 
 export default root;
