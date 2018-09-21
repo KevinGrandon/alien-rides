c3ea48d9576d0e8234a91da6d30e496113c191ad
Kevin Grandon <kevingrandon@yahoo.com>
2018-09-20 23:38:40 -0700

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 URL="$(now --public)";
 now alias set "$URL" alienrides.com
 now alias set "$URL" www.alienrides.com
-now remove my-app --yes --safe
+now alias set "$URL" alien-rides.now.sh
+now remove alien-rides --yes --safe
