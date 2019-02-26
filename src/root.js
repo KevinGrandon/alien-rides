52528ce9a7d4797c3b1dfb9a446ac7c127b660d0
Kevin Grandon <kevingrandon@yahoo.com>
2019-02-25 19:09:37 -0800

    Google tag

--- src/root.js
+++ src/root.js
@@ -6,6 +6,18 @@ import {Route, Switch} from 'fusion-plugin-react-router';
 import Home from './pages/home.js';
 import PageNotFound from './pages/pageNotFound.js';
 
+class AdComponent extends React.Component {
+  componentDidMount() {
+    (window.adsbygoogle = window.adsbygoogle || []).push({
+      google_ad_client: 'ca-pub-9060986956362826',
+      enable_page_level_ads: true,
+    });
+  }
+  render() {
+    return null;
+  }
+}
+
 const root = (
   <div>
     <Helmet>
@@ -14,7 +26,12 @@ const root = (
         name="description"
         content="AlienRides is promoting electric vehicle use including electric scooters, electric skateboards, electric unicycles and any kind of electric vehicle."
       />
+      <script
+        async
+        src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
     </Helmet>
+    <AdComponent />
     <Switch>
       <Route exact path="/" component={Home} />
       <Route component={PageNotFound} />
